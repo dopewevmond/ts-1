662fed247f649701a9185ab6607041e898d9bf98
dopewevmond <dopewevmond@gmail.com>
2022-11-09 13:27:53 +0000

    documenting the function

--- src/helpers/helpers.requestdetails.ts
+++ src/helpers/helpers.requestdetails.ts
@@ -1,5 +1,10 @@
 import { Request } from 'express'
 
+/**
+ * Gets details about a request which can be used for logging and analytics
+ * @param req The request object
+ * @returns method, path, IP address, time, query parameters, status code of  `req`
+ */
 function requestDetails(req: Request) {
   const time = new Date().toISOString()
   return `${req.method} ${req.path} -- ${req.ip} -- ${time} -- ${JSON.stringify(req.query)} -- ${req.statusCode}`
